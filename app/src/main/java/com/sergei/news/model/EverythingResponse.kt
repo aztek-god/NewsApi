0ebcb1599ead82fb374e0bee6c325a1d69a1af8d
sdvst <sdvstark@gmail.com>
2019-08-28 00:12:20 +0300

    Init commit

---
+++ app/src/main/java/com/sergei/news/model/EverythingResponse.kt
@@ -0,0 +1,39 @@
+package com.sergei.news.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class EverythingResponse(
+    @SerializedName("articles")
+    val articles: List<Article> = listOf(),
+    @SerializedName("status")
+    val status: String = "",
+    @SerializedName("totalResults")
+    val totalResults: Int = 0
+) {
+    data class Article(
+        @SerializedName("author")
+        val author: String = "",
+        @SerializedName("content")
+        val content: String = "",
+        @SerializedName("description")
+        val description: String = "",
+        @SerializedName("publishedAt")
+        val publishedAt: String = "",
+        @SerializedName("source")
+        val source: Source = Source(),
+        @SerializedName("title")
+        val title: String = "",
+        @SerializedName("url")
+        val url: String = "",
+        @SerializedName("urlToImage")
+        val urlToImage: String = ""
+    ) {
+        data class Source(
+            @SerializedName("id")
+            val id: String = "",
+            @SerializedName("name")
+            val name: String = ""
+        )
+    }
+}
